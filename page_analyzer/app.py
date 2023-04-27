d3e08d0e2cc5b335975a602c926f1823f3ec7048
KsyushaKI <oksanaakadem39@gmail.com>
2023-04-27 21:10:22 +0300

    step 2: add main html template

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from flask import render_template
+import os
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to Flask!'
+    return render_template('main/index.html')
